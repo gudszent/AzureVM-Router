87049cb582f2c867e15b6f5407bc40f7ec6c389c
gudszent <gudszent@gmail.com>
2021-12-16 11:54:00 +0100

    add fw and nat allow
    sudo -i iptables -A FORWARD -o eth0 -j ACCEPT
    sudo -i iptables -A FORWARD -i eth0 -j ACCEPT
    sudo -i iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE

--- LinuxRouter.sh
+++ LinuxRouter.sh
@@ -2,3 +2,6 @@
 # Enable IP forwarding
 sudo -i sysctl -w net.ipv4.ip_forward=1
 sudo sed -i "/net.ipv4.ip_forward=1/ s/# *//" /etc/sysctl.conf
+sudo -i iptables -A FORWARD -o eth0 -j ACCEPT
+sudo -i iptables -A FORWARD -i eth0 -j ACCEPT
+sudo -i iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
