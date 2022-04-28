8a259f84be0b6be462d557280c68f3d7f21f2caf
gudszent <gudszent@gmail.com>
2022-04-28 15:41:14 +0200

    Update LinuxRouter.sh

--- LinuxRouter.sh
+++ LinuxRouter.sh
@@ -5,5 +5,7 @@ sudo sed -i "/net.ipv4.ip_forward=1/ s/# *//" /etc/sysctl.conf
 sudo -i iptables -A FORWARD -o eth0 -j ACCEPT
 sudo -i iptables -A FORWARD -i eth0 -j ACCEPT
 sudo -i iptables -t nat -A POSTROUTING ! -d 10.0.0.0/8 -o eth0 -j MASQUERADE
-sudo apt update
-sudo apt install iptables-persistent -y
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+sudo apt-get update
+sudo apt-get -y install iptables-persistent
